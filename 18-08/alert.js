d01d22e7a9ee6cff0c12e6b5b753de325b7574bc
Pratik Jadhav <96625125+Pratik-Jadhav7@users.noreply.github.com>
2023-08-18 23:49:49 +0530

    Update alert.js

--- 18-08/alert.js
+++ 18-08/alert.js
@@ -1,2 +1,5 @@
 window.alert("Hello world!");
 alert("Hello world!");
+
+
+window.alert() instructs the browser to display a dialog with an optional message, and to wait until the user dismisses the dialog.
